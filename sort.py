cdba50bf3718f403ddee4e27b79f84a83c8801a6
Jan <jan.spoerer@whu.edu>
2018-09-21 12:10:10 +0200

    Adds a sorting algorithm

---
+++ sort.py
@@ -0,0 +1,20 @@
+# GH: vaheandonians
+
+def sort_my_array( array ):
+  print ( array )
+  j = 1
+  for j in range(j, len(array)):
+    key = array[j]
+    i = j - 1
+    while i >= 0 and array[i] > key:
+      array[i + 1] = array[i]
+      i = i - 1
+    array[i + 1] = key
+    print ( array )
+  return array
+
+array = [7, 2, 9, 4, 3, 1, 6, 5, 8]
+
+sort_my_array(array)
+
+print (array)
